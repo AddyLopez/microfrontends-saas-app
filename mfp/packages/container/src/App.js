b918aca0b40cd071fcf6a4e14206679e0f977b7f
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-08-23 00:52:43 -0600

    Add clarifying annotation

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
-import MarketingApp from "./components/MarketingApp"; // It's no longer necessary to import the mount function here in App.js
+import MarketingApp from "./components/MarketingApp";
+// It's no longer necessary to import the mount function here in App.js
+// This solution meets inflexible design requirement of near-zero coupling between container and child apps.
+// Container shouldn't assume child uses a particular framework. Any necessary communication should be done with callbacks or simple events
 
 const App = () => {
   return (
