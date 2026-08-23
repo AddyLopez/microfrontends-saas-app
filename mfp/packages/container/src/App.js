426b7c22b400834cc85ba1c6b87b2a993ae91fb7
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-08-23 00:43:53 -0600

    import mount function and annotate its purpose

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { mount } from "marketing/MarketingApp";
+// Why import mount funtion (rather than, say, a React Marketing component)? Its purpose is to meet inflexible design requirement of near-zero coupling between container and child apps.
+// Container shouldn't assume chile uses a particular framework. Any necessary communication should be done with callbacks or simple events
+
+console.log(mount);
 
 const App = () => {
   return <h1>Hello, from Container!</h1>;
