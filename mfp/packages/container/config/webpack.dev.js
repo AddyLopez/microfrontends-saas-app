94136a5f856d1af9add92225c6d890de8b09043c
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-08-23 00:43:22 -0600

    Configure Module Federation Plugin for container

--- mfp/packages/container/config/webpack.dev.js
+++ mfp/packages/container/config/webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge"); // Function enables merging of config in common file with config in this development file
 const HtmlWebpackPlugin = require("html-webpack-plugin"); // Will inject proper script tags to html file
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 
 // Development-specific Webpack configuration
@@ -12,6 +13,12 @@ const devConfig = {
     },
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+      },
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
