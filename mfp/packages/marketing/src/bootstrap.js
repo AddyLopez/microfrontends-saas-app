92aa0fbe69c240f90ad9ff95a4b2ecaf22c97176
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-08-22 23:40:11 -0600

    Render App component

--- mfp/packages/marketing/src/bootstrap.js
+++ mfp/packages/marketing/src/bootstrap.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
 // Mount function to start up the app. Renders JSX in given element.
 const mount = (element) => {
-  ReactDOM.render(<h1>Marketing Mounted!</h1>, element);
+  ReactDOM.render(<App />, element);
 };
 
 // If in development and in isolation, call mount immediately
