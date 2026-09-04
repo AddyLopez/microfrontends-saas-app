88a6420cd7e26f57d486ccc5db4ad1484d5fbb6a
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-09-04 00:05:07 -0600

    Use prefix to prevent CSS class name collisions

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,17 +2,28 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 // It's no longer necessary to import the mount function here in App.js
 // This solution meets inflexible design requirement of near-zero coupling between container and child apps.
 // Container shouldn't assume child uses a particular framework. Any necessary communication should be done with callbacks or simple events
 
+// productionPrefix is provided to future-proof application from CSS class name collisions in production.
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
