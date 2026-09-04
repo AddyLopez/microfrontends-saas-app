661076684ae9b72a7efa5cdd7e3815ade1996f8f
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-09-03 22:31:44 -0600

    Import Header component and react browser router

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 // It's no longer necessary to import the mount function here in App.js
 // This solution meets inflexible design requirement of near-zero coupling between container and child apps.
 // Container shouldn't assume child uses a particular framework. Any necessary communication should be done with callbacks or simple events
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello, from Container!!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
 
