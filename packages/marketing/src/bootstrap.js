6f49fdbadd85ffbc7518b4fc67d7e066f2de8f62
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-09-05 22:30:29 -0600

    Pass memory history off as props to App component

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createMemoryHistory } from "history"; // Not imported from react-router-dom because React Router uses this library.
 
 // Mount function to start up the app. Renders JSX in given element.
 const mount = (element) => {
-  ReactDOM.render(<App />, element);
+  const history = createMemoryHistory(); // Routing for subapps will rely on memory history, while routing for container app will rely on browser history.
+
+  ReactDOM.render(<App history={history} />, element);
 };
 
 // If in development and in isolation, call mount immediately
