cc3b679b72bdd8b25cd9b9a60cfd2bf6dd5e74fb
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-09-06 13:48:37 -0600

    Provide empty options object to prevent error

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -7,9 +7,12 @@ import { createMemoryHistory } from "history"; // Not imported from react-router
 const mount = (element, { onNavigate }) => {
   const history = createMemoryHistory(); // Routing for subapps will rely on memory history, while routing for container app will rely on browser history.
 
-  // "listen" event listener exists on history object and will call any function provided as argument.
-  // onNavigate is invoked whenever memory history's URL (path) changes and communicates up to the container app.
-  history.listen(onNavigate);
+  // Make sure onNavigate is only invoked if it has been passed down from container app
+  if (onNavigate) {
+    // "listen" event listener exists on history object and will call any function provided as argument.
+    // onNavigate is invoked whenever memory history's URL (path) changes and communicates up to the container app.
+    history.listen(onNavigate);
+  }
 
   ReactDOM.render(<App history={history} />, element);
 };
@@ -19,7 +22,7 @@ if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {}); // Empty options object provided as second argument to prevent error when running Marketing subapp in isolation
   }
 }
 
