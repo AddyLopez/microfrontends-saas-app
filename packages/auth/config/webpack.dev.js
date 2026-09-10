3368282f217d7f27cce95b08c6cb14e3abcc491b
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-09-09 23:11:47 -0600

    configure webpack.dev.js for auth subapp
    Updated: port number, name, alias under exposes property

---
+++ packages/auth/config/webpack.dev.js
@@ -0,0 +1,31 @@
+const { merge } = require("webpack-merge"); // Function enables merging of config in common file with config in this development file
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // Will inject proper script tags to html file
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json"); // Used to facilitate updating of shared dependencies. Here, the JSON gets parsed as a regular JS object
+
+// Development-specific Webpack configuration
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8082,
+    historyApiFallback: {
+      index: "/index.html",
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "auth",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./AuthApp": "./src/bootstrap",
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig); // Merges the configuration files. Second argument will override or take priority over similar options assigned to first argument
