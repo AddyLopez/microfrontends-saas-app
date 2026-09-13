a80b6337be3318a4360fe1f9bd78f6f417308c79
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-09-12 23:04:46 -0600

    Reconfigure routes in order to pass onSignIn props

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -14,14 +14,18 @@ const generateClassName = createGenerateClassName({
 });
 
 // Using Router rather than BrowserRouter enables use of Memory History for subapps while Browser History is used for the container app
-const App = ({ history }) => {
+const App = ({ history, onSignIn }) => {
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
           <Switch>
-            <Route path="/auth/signin" component={Signin} />
-            <Route path="/auth/signup" component={Signup} />
+            <Route path="/auth/signin">
+              <Signin onSignIn={onSignIn} />
+            </Route>
+            <Route path="/auth/signup">
+              <Signup onSignIn={onSignIn} />
+            </Route>
           </Switch>
         </Router>
       </StylesProvider>
