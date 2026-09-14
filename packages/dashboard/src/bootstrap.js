12b7a3b533d60166a4a5507dbe566c95ddb2d31c
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-09-14 00:12:35 -0600

    fix typo to get Dashboard to render

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import Dashboard from "./components/Dashboard.vue";
 
-// Mount function to start up the app. Renders JSX in given element
+// Mount function to start up the app.
 const mount = (element) => {
   const app = createApp(Dashboard);
   app.mount(element); // This mount function on app is specific to Vue and unrelated to the other mount function. It tells Vue to show a component inside the DOM.
@@ -9,7 +9,7 @@ const mount = (element) => {
 
 // If in development and in isolation, call mount immediately
 if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector("#dashboard-dev-root");
+  const devRoot = document.querySelector("#_dashboard-dev-root");
 
   if (devRoot) {
     mount(devRoot);
