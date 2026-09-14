9aaafd5b4d5824c8dcfd8af8caaf14c1fa3ed78d
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-09-13 23:28:33 -0600

    Update configuration for Vue in Dashboard subapp

---
+++ packages/dashboard/config/webpack.common.js
@@ -0,0 +1,43 @@
+// loader is defined in rules array. purpose of a loader, such as Babel, is to tell Webpack to process different files
+// test property's value is a regex which tells Babel to process any file imported with extension of .mjs or .js
+// some rules are configured specifically for Vue (images, fonts, css, .vue files, etc.)
+
+const { VueLoaderPlugin } = require("vue-loader");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  resolve: {
+    extensions: [".js", ".vue"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|woff|svg|eot|ttf)$/i,
+        use: [{ loader: "file-loader" }],
+      },
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.scss|\.css$/,
+        use: ["vue-style-loader", "style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
+};
