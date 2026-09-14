ed7f8be76501c07fa375ef2874551df6ebd18b24
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-09-13 23:44:57 -0600

    Configure for Vue, eliminate references to history

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -1,47 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { createMemoryHistory, createBrowserHistory } from "history"; // Not imported from react-router-dom because React Router uses this library.
+import { createApp } from "vue";
+import Dashboard from "./components/Dashboard.vue";
 
 // Mount function to start up the app. Renders JSX in given element
-const mount = (
-  element,
-  { onSignIn, onNavigate, defaultHistory, initialPath },
-) => {
-  const history =
-    defaultHistory ||
-    createMemoryHistory({
-      initialEntries: [initialPath], // gives memory history initial state to prevent a routing bug
-    }); // defaultHistory is used when running Auth app in isolation in development. Otherwise, routing for subapps will rely on memory history, while routing for container app will rely on browser history.
-
-  // Make sure onNavigate is only invoked if it has been passed down from container app
-  if (onNavigate) {
-    // "listen" event listener exists on history object and will call any function provided as argument.
-    // onNavigate is invoked whenever memory history's URL (path) changes and communicates up to the container app.
-    history.listen(onNavigate);
-  }
-
-  ReactDOM.render(<App history={history} onSignIn={onSignIn} />, element);
-
-  // mount now returns a function. facilitates container to subapp (child) communication
-  return {
-    onParentNavigate({ pathname: nextPathname }) {
-      const { pathname } = history.location; // destructures pathname from memory history's location object
-
-      // prevents circular logic between browser history and memory history: if the two paths are not the same, then the navigation paths need to be synced.
-      if (pathname !== nextPathname) {
-        history.push(nextPathname); // syncs browser history detected in container with memory history in subapp
-      }
-    },
-  };
+const mount = (element) => {
+  const app = createApp(Dashboard);
+  app.mount(element); // This mount function on app is specific to Vue and unrelated to the other mount function. It tells Vue to show a component inside the DOM.
 };
 
 // If in development and in isolation, call mount immediately
 if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector("#_auth-dev-root");
+  const devRoot = document.querySelector("#dashboard-dev-root");
 
   if (devRoot) {
-    mount(devRoot, { defaultHistory: createBrowserHistory() }); // Option provided of defaultHistory. Its value is an instance of Browser History. Its goal is to improve development experience when running this subapp in isolation by providing routing paths via browser history in the URL, not just in memory history.
+    mount(devRoot);
   }
 }
 
